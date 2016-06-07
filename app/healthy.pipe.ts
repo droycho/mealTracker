7ac9863d19f5cebd8562c8d37bb5e47d3fc3dc0e
sea413 <seanmp42@gmail.com>
2016-06-07 13:54:57 -0700

    added comments, restructured pipe.

--- app/healthy.pipe.ts
+++ app/healthy.pipe.ts
@@ -10,19 +10,34 @@ export class HealthyPipe implements PipeTransform {
 
     var desiredHealthyState = args[0];
     if(desiredHealthyState === "healthy") {
-      return input.filter((meal) => {
+      return input.filter(function(meal) {
         if(meal.calories < 500) {
-        return meal.healthy;}
+          return true;
+        }
       });
     } else if (desiredHealthyState === "notHealthy") {
-      return input.filter((meal) => {
+      return input.filter(function(meal){
         if(meal.calories > 500) {
-        return !meal.healthy;}
+          return true;
+        }
       });
     } else {
       return input;
     }
   }
 }
+// export class HealthyPipe implements PipeTransform {
+//   transform(mealList: Meal[], allTheColonSeparatedValues) {
+//
+//     var menuChoice = allTheColonSeparatedValues[0];
+//     if(menuChoice === "healthy") { //if the user wants meals < 500 calories
+//       return //an array of all meals we want to see (<500)
+//     } else if (menuChoice === "notHealthy") {
+//       return //an array of all meals >500 calories
+//     } else { //if they hit "show all" instead of healthy/not healthy.
+//       return mealList; //return all meals in an array, unchanged.
+//     }
+//   }
+// }
 
 //items will only show in healthy if check box is selected, however, will not show in unhealthy if calories are less than 500.
