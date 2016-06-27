5c3bc62c6262ca259c6d2f91a7f200cc5d66e33c
droycho <djroychoudhury@yahoo.com>
2016-06-27 00:37:44 -0700

    clean up comments and logs.

--- app/new-meal.component.ts
+++ app/new-meal.component.ts
@@ -27,13 +27,9 @@ export class NewMealComponent {
     var details = userDetails.value;
     var calories = parseInt(userCalories.value);
     var meal = new Meal(name, details, calories, 0)
-    // var meal = new Meal(userName.value, userDetails.value, parseInt(userCalories.value), 0);
-    console.log(meal);
     this.onSubmitNewMeal.emit(meal);
     userName.value = "";
     userDetails.value = "";
     userCalories.value = "";
   }
 }
-
-//variable meal is picking up values, but it is not displaying as clickable link in the target.
